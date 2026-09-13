c51453abd0842242a46b60fddfa764b70ea93959
ZakaCoding <zakanoor@outlook.co.id>
2026-09-13 17:30:55 +0700

    Updates

--- assets/lottie-react.esm-95d4f5af.js
+++ assets/lottie-react.esm-95d4f5af.js
@@ -1,4 +1,4 @@
-import{r as reactExports}from"./index-a05a761f.js";/*! *****************************************************************************
+import{r as reactExports}from"./index-1ebd2d76.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
