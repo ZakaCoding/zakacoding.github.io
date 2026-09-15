ead7e5dfb5881b1e7546bc6dc618e5d970347bac
Zaka Noor <zakanoor@outlook.co.id>
2026-09-15 18:30:57 +0000

    Updates

--- assets/lottie-react.esm-ae2f88d2.js
+++ assets/lottie-react.esm-ae2f88d2.js
@@ -1,4 +1,4 @@
-import{r as reactExports}from"./index-59cbac8a.js";/*! *****************************************************************************
+import{r as reactExports}from"./index-6e7ee2c7.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
